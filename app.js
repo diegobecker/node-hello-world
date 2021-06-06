fdcb4c84f261105320e1a9d7f967515ecb9cc9d1
Diego Becker <diegobecker00@gmail.com>
2021-06-06 19:12:24 -0300

    Add mysql example

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'admin',
+  password: 'mudar1234',
+  database: 'teste'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL Server!');
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
